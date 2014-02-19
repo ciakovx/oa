e5a65a094c6c57baa36e1846dc9a9c08c4584a24
ciakovx <clarke.iak@gmail.com>
2014-02-19 13:04:58 -0600

    Rewrites code entirely. The first one did not account for capitalization and so missed about 40 values.
    This one puts each journal list into a dataframe, capitalizes them, then uses the intersect function rather than the %in% function.

--- AllOAJournalsv1.R
+++ AllOAJournalsv1.R
@@ -1,16 +1,32 @@
-# This looks at the complete list of OA journals and compares it with the DOAJ list to return a master list of OA journals listed in the AA database.
-# I ended up doing it in Excel because this one omitted specifically foreign language journals and journals with unusual capitalization
-# Need to figure out if that is avoidable
-# But the difference was negligible (This code did not pick up 40 journals)
+# Get all DOAJ journals on the AA journal list
 
 
-all.journals <- read.csv(file=file.path(getwd(), "Copy of Journals_AAD2011.csv"))
-all.oa <- all.journals$AAD.2011.Journal.List %in% doaj$Title # full list of journals in the list that match DOAJ Titles
-all.oa <- all.journals[all.oa, ] # subset that dataframe with matches
-names(all.oa) <- c("Journal", "Discipline") #rename columns
-all.oa <- as.character(all.oa$Journal)
-str(all.oa)
-oalist <- unique(all.oa) # get unique values
-all.oa <- as.character(oalist) # final vector 
-write.csv(all.oa, file="alloa.csv")
+#first 
+setwd("C:/Users/clarke/Desktop/AcademicAnalyticsData") #set working directory
+#then read in 
+doaj <- read.csv(file="C:/Users/clarke/Desktop/AcademicAnalyticsData/DOAJ.csv") # import directory of OA journals file
+all.journals <- read.csv(file=file.path(getwd(), "Copy of Journals_AAD2011.csv")) # read in the file
 
+
+a <- data.frame(doaj$Title) # get list of DOAJ titles
+a <- factor(a$doaj.Title) # convert to factor
+a <- toupper(a) # convert to upper case
+dupe.a <- duplicated(a) # logical vector of duplicates
+a <- a[!dupe.a] # return all DOAJ titles
+
+b <- data.frame(all.journals$AAD.2011.Journal.List) # get list of AA titles
+b <- factor(b$all.journals.AAD.2011.Journal.List) # convert to factor
+b <- toupper(b) # convert to upper case
+dupe.b <- duplicated(b) # logical vector of duplicates
+b <- b[!dupe.b] # return all AA journals
+
+c <- intersect(a,b) # intersection of AA & DOAJ journals
+
+## This was a first go, but because of the capitalization issues, it was a no go.
+# matches <- all.journals$AAD.2011.Journal.List %in% doaj$Title # logical vector of journals in the list that match DOAJ Titles
+# all.oa <- all.journals[matches, ] # create a subset of all.journals list according to the matches vector
+# names(all.oa) <- c("Journal", "Discipline") #rename columns
+# all.oa2 <- factor(all.oa$Journal) # remove factors (journals) that were excluded from subset 
+# oalist <- unique(all.oa) # get unique values
+# all.oa <- as.character(oalist) # final vector 
+# write.csv(all.oa, file="alloa.csv")
