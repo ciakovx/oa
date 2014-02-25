1e85f7a64f0cfaf83c91b9a5d907d63f04ab04c0
ciakovx <clarke.iak@gmail.com>
2014-02-25 09:27:46 -0600

    Add scopus list to the intersection test. Change the "all.journals" variable to "aa.journals". Add count of intersections

--- AllOAJournalsv1.R
+++ AllOAJournalsv1.R
@@ -1,11 +1,10 @@
-# Get all DOAJ journals on the AA journal list
+scopus <- read.csv(file=file.path(getwd(), "Scopus.csv")) # read in scopus file 
+names(scopus) <- c("Title", "Print.issn", "E.issn") # change column names
 
 
-#first 
-setwd("C:/Users/clarke/Desktop/AcademicAnalyticsData") #set working directory
-#then read in 
-doaj <- read.csv(file="C:/Users/clarke/Desktop/AcademicAnalyticsData/DOAJ.csv") # import directory of OA journals file
-aa.journals <- read.csv(file=file.path(getwd(), "Copy of Journals_AAD2011.csv")) # read in the file
+doaj <- read.csv(file="C:/Users/clarke/Desktop/AcademicAnalyticsData/DOAJ.csv") # read in the DOAJ file
+aa.journals <- read.csv(file=file.path(getwd(), "Copy of Journals_AAD2011.csv")) # read in the Academic Analytics file
+
 
 
 doaj.list <- data.frame(doaj$Title) # get list of DOAJ titles
@@ -14,19 +13,21 @@ doaj.list <- toupper(doaj.list) # convert to upper case
 dupe.a <- duplicated(doaj.list) # logical vector of duplicates
 doaj.list <- doaj.list[!dupe.a] # return all DOAJ titles
 
-aa.list <- data.frame(all.journals$AAD.2011.Journal.List) # get list of AA titles
-aa.list <- factor(aa.list$all.journals.AAD.2011.Journal.List) # convert to factor
+aa.list <- data.frame(aa.journals$AAD.2011.Journal.List) # get list of AA titles
+aa.list <- factor(aa.list$aa.journals.AAD.2011.Journal.List) # convert to factor
 aa.list <- toupper(aa.list) # convert to upper case
 dupe.b <- duplicated(aa.list) # logical vector of duplicates
 aa.list <- aa.list[!dupe.b] # return all AA journals
 
-aa.doaj <- intersect(doaj.list,aa.list) # intersection of AA & DOAJ journals
 
-## This was a first go, but because of the capitalization issues, it was a no go.
-# matches <- all.journals$AAD.2011.Journal.List %in% doaj$Title # logical vector of journals in the list that match DOAJ Titles
-# all.oa <- all.journals[matches, ] # create a subset of all.journals list according to the matches vector
-# names(all.oa) <- c("Journal", "Discipline") #rename columns
-# all.oa2 <- factor(all.oa$Journal) # remove factors (journals) that were excluded from subset 
-# oalist <- unique(all.oa) # get unique values
-# all.oa <- as.character(oalist) # final vector 
-# write.csv(all.oa, file="alloa.csv")
+scopus.list <- data.frame(scopus$Title) # get list of AA titles
+scopus.list <- factor(scopus$Title) # convert to factor
+scopus.list <- toupper(scopus.list) # convert to upper case
+dupe.c <- duplicated(scopus.list) # logical vector of duplicates
+scopus.list <- scopus.list[!dupe.c] # return all AA journals
+
+
+
+aa.doaj <- intersect(doaj.list,aa.list) # intersection of AA & DOAJ journals (1,117)
+aa.scopus <- intersect(aa.list, scopus.list) # intersection of AA & Scopus (10,745)
+scopus.doaj <- intersect(scopus.list, doaj.list) # intersection of Scopus & DOAJ (2,015)
