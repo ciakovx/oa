4c8e04c3d506526fc8ed0ff443feb26392056343
ciakovx <clarke.iak@gmail.com>
2014-02-25 17:53:00 -0600

    Accounting for trailing spaces. Modifies each function to add the str-trim function. Notice the changes in the intersection counts. Notice there were NO changes in the scopus.aa intersection, indicating that all the spacing trouble came from the DOAJ list. Lesson: clean your character strings of trailing spaces before any analysis.

--- AllOAJournalsv1.R
+++ AllOAJournalsv1.R
@@ -1,3 +1,9 @@
+# Need stringr package:
+library(stringr)
+
+setwd("C:/Users/clarke/Desktop/AcademicAnalyticsData") #set working directory
+
+
 scopus <- read.csv(file=file.path(getwd(), "Scopus.csv")) # read in scopus file 
 names(scopus) <- c("Title", "Print.issn", "E.issn") # change column names
 
@@ -9,29 +15,36 @@ aa.journals <- read.csv(file=file.path(getwd(), "Copy of Journals_AAD2011.csv"))
 
 doaj.titles <- data.frame(doaj$Title) # get list of DOAJ titles
 doaj.titles <- factor(doaj.titles$doaj.Title) # convert to factor
+doaj.titles <- str_trim(doaj.titles, side = "both") # trim extra spaces on doaj list
 doaj.titles <- toupper(doaj.titles) # convert to upper case
 dupe.a <- duplicated(doaj.titles) # logical vector of duplicates
-doaj.list <- doaj.titles[!dupe.a] # return all DOAJ titles as characters, in caps, without duplicates
-doaj.list.dupes <- doaj.titles[dupe.a] # return all duplicated journals from the DOAJ list (14)
+doaj.list <- doaj.titles[!dupe.a] # return all DOAJ titles as characters, in caps, without duplicates (9,786)
+doaj.list.dupes <- doaj.titles[dupe.a] # return all duplicated journals from the DOAJ list (18)
+
 
 aa.titles <- data.frame(aa.journals$AAD.2011.Journal.List) # get list of AA titles
 aa.titles <- factor(aa.titles$aa.journals.AAD.2011.Journal.List) # convert to factor
+aa.titles <- str_trim(aa.list, side = "both") # trim extra spaces on aa list
 aa.titles <- toupper(aa.titles) # convert to upper case
 dupe.b <- duplicated(aa.titles) # logical vector of duplicates
-aa.list <- aa.titles[!dupe.b] # return all AA journals as characters, in caps, without duplicates
+aa.list <- aa.titles[!dupe.b] # return all AA journals as characters, in caps, without duplicates (14,586)
 aa.list.dupes <- aa.titles[dupe.b] # return all duplicated journals from the AA list (203,883)
 
 scopus.titles <- data.frame(scopus$Title) # get list of Scopus titles
 scopus.titles <- factor(scopus$Title) # convert to factor
+scopus.titles <- str_trim(scopus.titles, side = "both") # trim extra spaces on doaj list
 scopus.titles <- toupper(scopus.titles) # convert to upper case
 dupe.c <- duplicated(scopus.titles) # logical vector of duplicates
-scopus.list <- scopus.titles[!dupe.c] # return all Scopus journals as characters, in caps, without duplicates
+scopus.list <- scopus.titles[!dupe.c] # return all Scopus journals as characters, in caps, without duplicates (34,275)
 scopus.list.dupes <- scopus.titles[dupe.c] # return all duplicated journals from the Scopus list (156)
 
 
 
-aa.doaj <- intersect(doaj.list,aa.list) # intersection of AA & DOAJ journals (1,117)
-aa.scopus <- intersect(aa.list, scopus.list) # intersection of AA & Scopus (10,745)
-scopus.doaj <- intersect(scopus.list, doaj.list) # intersection of Scopus & DOAJ (2,015)
+# Updated totals after applying trim
+aa.doaj <- intersect(doaj.list,aa.list) # intersection of AA & DOAJ journals (1,198)
+aa.scopus <- intersect(aa.list, scopus.list) # intersection of AA & Scopus (10,745) #the same: this proves it was DOAJ
+scopus.doaj <- intersect(scopus.list, doaj.list) # intersection of Scopus & DOAJ (2,118)
 
-doaj.total <- intersect(aa.doaj, scopus.doaj) # DOAJ titles in both AA & Scopus (796)
+doaj.total <- intersect(aa.doaj, scopus.doaj) # DOAJ titles in both AA & Scopus (837)
+
+
