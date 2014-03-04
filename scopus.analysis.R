1288d4b2fd7adde6e9d42072422305d1e3f5c360
ciakovx <clarke.iak@gmail.com>
2014-03-04 15:33:41 -0600

    Clean scopus titles and type with same procedure as other dfs. Run table, forget running the percentages (94% for one and 2% all the rest is useless, but keep this function in mind for other dfs).

--- scopus.analysis.R
+++ scopus.analysis.R
@@ -2,18 +2,23 @@
 library(stringr)
 
 setwd("J:/Escience/AcademicAnalytics") #set working directory
-
-
-# Clean the Type column
 scopus <- read.csv(file=file.path(getwd(), "data", "2014-02-02", "Scopus", "Scopus.selected.csv")) # read in the Academic Analytics file
-scopus$Type <- str_trim(scopus$Type) # trim the whitespace (for Book Series)
-scopus$Type[scopus$Type == "Trade journal"] <- "Trade Journal" # capitalize j in trade journal
+
+# Clean the data: removing whitespace and converting to uppercase
+scopus.titles <- data.frame(scopus$Title, scopus$Type) # get list of Scopus titles
+scopus.titles$scopus.Title <- factor(scopus$Title) # convert to factor
+scopus.titles$scopus.Title <- str_trim(scopus.titles$scopus.Title, side = "both") # trim extra spaces on doaj list
+scopus.titles$scopus.Title <- toupper(scopus.titles$scopus.Title) # convert to upper case
+scopus.titles$scopus.Type <- factor(scopus$Type) # convert to factor
+scopus.titles$scopus.Type <- str_trim(scopus.titles$scopus.Type, side = "both") # trim the whitespace (for Book Series)
+scopus.titles$scopus.Type <- toupper(scopus.titles$scopus.Type) # convert to upper case
+dupe.c <- duplicated(scopus.titles$scopus.Title) # logical vector of duplicates
+scopus.list <- scopus.titles[!dupe.c,]
+scopus.list.dupes <- scopus.titles[dupe.c,] # return all duplicated journals from the Scopus list (156)
 
 # Get a table of Type Freq & Percentage
-scopus.type.table <- as.data.frame(table(scopus$Type)) # Get the Freq for Type
-scopus.type.counts <- as.data.frame(round(prop.table(table(scopus$Type)), digits = 3)) # calculates the frequency and percent of each type
-scopus.type.tot <- cbind(scopus.type.table, as.numeric(scopus.type.counts$Freq)) # Bind together the columns
-names(scopus.type.tot) <- c("Type", "Count", "PercentTotal")
+scopus.type.table <- as.data.frame(table(scopus.list$scopus.Type)) # Get the Freq for Type
+names(scopus.type.table) <- c("Type", "Count")
 
 # Write CSVs
-write.csv(scopus.type.tot, file=file.path(getwd(), "results", "2014-02-26", "tables", "Scopus", "scopus.type.tot.csv"))
+write.csv(scopus.type.table, file=file.path(getwd(), "results", "2014-02-26", "tables", "Scopus", "scopus.type.table.csv"))
