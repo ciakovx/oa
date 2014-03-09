ffe17cacf46a0f71f574e8bfd8057cd2da345035
ciakovx <clarke.iak@gmail.com>
2014-03-09 14:01:27 -0500

    Makes ArtCounts into a function. Makes the plot into a function. Adds the theme_bw to the plot. Changes "Discipline" into "Department"

--- PlotAllDepts.R
+++ PlotAllDepts.R
@@ -2,29 +2,42 @@
 depts.compl <- depts[complete.cases(depts),]
 depts.incompl <- depts[!complete.cases(depts),]
 
-#Create dataframe of total articles published by discipline
-ArtCounts <- tapply(depts.compl$ArticlesPublished,depts.compl$Discipline,sum) #create table of sums of articles applied to the ArticlesPublished column
-ArtCounts <- data.frame("ArticlesPublished"=ArtCounts[complete.cases(ArtCounts)]) #create dataframe of that table
-ArtCounts <- data.frame("Discipline"=as.character(rownames(ArtCounts)), "ArticlesPublished"=as.integer(ArtCounts$ArticlesPublished)) #make the rownames of that df into a variable
 
+ArticleCounts <- function(deptscompldata) {
+  # Create dataframe of total articles published by discipline
+  #
+  # Arg: The depts data created by the oa.journals function with NAs removed--cleaned with depts[complete.cases(depts),]
+  #
+  #Returns: A dataframe with two variables: The discipline, and the total number of articles published in OA journals
+  ArtCounts <- tapply(deptscompldata$ArticlesPublished,depts.compl$Discipline,sum) #create table of sums of articles applied to the ArticlesPublished column
+  ArtCounts <- data.frame("ArticlesPublished"=ArtCounts[complete.cases(ArtCounts)]) #create dataframe of that table
+  ArtCounts <- data.frame("Department"=as.character(rownames(ArtCounts)), "ArticlesPublished"=as.integer(ArtCounts$ArticlesPublished)) #make the rownames of that df into a variable
+}
+ArtCounts <- ArticleCounts(depts.compl)
 
 library(ggplot2) # Load ggplot package
 
 
 #plot complete depts (ggplot)
-ArtCounts$Journals.ordered <- reorder(ArtCounts$Discipline, ArtCounts$ArticlesPublished) #sort Discipline by Articles Published
-pth <- pth <- file.path(getwd(), "results", "2014-02-26", "plots") # set a location for plots to be saved
-compl.depts.plot <- ggplot(data=ArtCounts) +
-  geom_bar(aes(x=Journals.ordered,y=ArticlesPublished),fill="orange",color="black",stat="identity") +
-  coord_flip() +
-  geom_text(aes(x=Discipline, y=ArticlesPublished, label=ArticlesPublished), hjust = -0.5, size=6) + #set text labels
-  ggtitle(label="Total Article Counts in Open Access Publications by Department, UTA 2004-2011") +
-  ylab("Number of Articles Published") +
-  xlab("Department") +
-  theme(text = element_text(size=20))
-print(compl.depts.plot)
+PlotAllDepts <- function(articlecountsdata) {
+  # Plots a total article count for each department and saves it into the specified directory
+  #
+  # Arg: Count of articles published, aggregated by discipline
+  
+  ArtCounts$Journals.ordered <- reorder(articlecountsdata$Department, articlecountsdata$ArticlesPublished) #sort Department by Articles Published
+  #pth <- pth <- file.path(getwd(), "results", "2014-03-08", "plots") # set a location for plots to be saved
+  compl.depts.plot <- ggplot(data=ArtCounts) +
+    geom_bar(aes(x=Journals.ordered, y=ArticlesPublished), fill="orange", color="black", stat="identity") +
+    coord_flip() +
+    geom_text(aes(x=Department, y=ArticlesPublished, label=ArticlesPublished), hjust = -0.5, size=6) + #set text labels
+    ggtitle(label="Total Article Counts in Open Access Publications by Department, UTA 2004-2011") +
+    ylab("Number of Articles Published") +
+    xlab("Department") +
+    theme(text = element_text(size=20)) + # Increase font size
+    theme_bw() # Get rid of grayscale background
 ggsave("AllDepts.png", path=pth, width=15, height=15) #save files 
-
+}
+PlotAllDepts(ArtCounts)
 
 #plot top 20 publishers (ggplot)
 #Tabulate by publisher
