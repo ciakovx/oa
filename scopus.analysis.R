95ac7e712118b798eb083a194a0a70b066864755
ciakovx <clarke.iak@gmail.com>
2014-03-13 12:08:58 -0500

    Comparing scopus vs. aa.

--- scopus.analysis.R
+++ scopus.analysis.R
@@ -4,20 +4,42 @@ library(stringr)
 setwd("J:/Escience/AcademicAnalytics") #set working directory
 scopus <- read.csv(file=file.path(getwd(), "data", "2014-02-02", "Scopus", "Scopus.selected.csv")) # read in the Academic Analytics file
 
-# Clean the data: removing whitespace and converting to uppercase
-scopus.titles <- data.frame(scopus$Title, scopus$Type) # get list of Scopus titles
-scopus.titles$scopus.Title <- factor(scopus$Title) # convert to factor
-scopus.titles$scopus.Title <- str_trim(scopus.titles$scopus.Title, side = "both") # trim extra spaces on doaj list
-scopus.titles$scopus.Title <- toupper(scopus.titles$scopus.Title) # convert to upper case
-scopus.titles$scopus.Type <- factor(scopus$Type) # convert to factor
-scopus.titles$scopus.Type <- str_trim(scopus.titles$scopus.Type, side = "both") # trim the whitespace (for Book Series)
-scopus.titles$scopus.Type <- toupper(scopus.titles$scopus.Type) # convert to upper case
-dupe.c <- duplicated(scopus.titles$scopus.Title) # logical vector of duplicates
-scopus.list <- scopus.titles[!dupe.c,]
-scopus.list.dupes <- scopus.titles[dupe.c,] # return all duplicated journals from the Scopus list (156)
+clean.scopus <- function(scopusdata) {
+  # Get a list of all journals indexed by Scopus. 
+  # 
+  # Arg: The raw Scopus journal data
+  # 
+  # Returns: A dataframe of Scopus journals and types, capitalized and with duplicates eliminated
+  
+  scopus.titles <- data.frame("Title"=str_trim(factor(toupper(scopusdata$Title)), side = "both"), "Type"=str_trim(factor(toupper(scopusdata$Type)), side = "both")) # get list of Scopus titles
+  dupe <- duplicated(scopus.titles$Title) # logical vector of duplicates
+  scopus.list <- scopus.titles[!dupe,] # Get all values that are not duplicated
+  scopus.list$Title <- as.character(scopus.list$Title) # Convert Title from factor to character
+  return(scopus.list)
+}
+scopus.list <- clean.scopus(scopus)
+
+aa.data <- function(aadata) {
+  # Get a list of all journals indexed by Academic Analytics. This is used later in an intersection function to analyze which DOAJ titles have punctuation problems inside the string
+  # 
+  # Arg: The raw Academic Analytics journal data
+  # 
+  # Returns: A character string of 14,586 Academic Analytics data, capitalized and with duplicates eliminated
+  
+  aa.titles <- data.frame("Title"=str_trim(factor(toupper(aadata$AAD.2011.Journal.List)))) # get list of AA titles
+  dupe <- duplicated(aa.titles) # logical vector of duplicates
+  aa.list <- aa.titles[!dupe,] # return all AA journals as characters, in caps, without duplicates (14,586)
+  aa.list <- as.character(aa.list) # convert from factor to character string
+  #aa.list.dupes <- aa.titles[dupe] # return all duplicated journals from the AA list (203,883)
+  return(aa.list)
+}
+aa.list <- aa.data(aa.journals)
+
+aa.scopus <- intersect(scopus.list$Title, aa.list))
+
 
 # Get a table of Type Freq & Percentage
-scopus.type.table <- as.data.frame(table(scopus.list$scopus.Type)) # Get the Freq for Type
+scopus.type.table <- as.data.frame(table(scopus.list$Type)) # Get the Freq for Type
 names(scopus.type.table) <- c("Type", "Count")
 
 # Write CSVs
